81fbd2d5a9c3ae507f124c87ee29d922260d9c92
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-11-25 09:46:12 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
-function validateFields(){    
-    const emailValid = isEmailValid();
-    document.querySelector(".btRecuperarSenha").disabled = !emailValid; // Habilita/Desabilita campo email
-    // se verdadeiro, entao habilita o botão de recuperar senha
-    // se falso, entao desabilita o botão de recuperar senha
-    const passwordValid = isPasswordValid();
-    document.querySelector(".btEntrar").disabled = !emailValid || !passwordValid; // Habilita/Desabilita campo senha
-    // se verdadeiro, entao habilita o botão de entrar
-    // se falso, entao desabilita o botão de entrar
+function onChangeEmail(){    
+    toggleButtonsDisable(); //Utilizando a função de validação de botões
+    toggleEmailErros(); //Utilizando a função de validação de erros no email
+}
+
+function onChangePassword(){
+    toggleButtonsDisable(); //Utilizando a função de validação de botões
+    togglePasswordError(); //Utilizando a função de validação de erros na senha
 }
 
 function isEmailValid(){ // Validar campo email    
@@ -15,6 +14,45 @@ function isEmailValid(){ // Validar campo email
         return false; //Retorna falso       
     }
     return validateEmail(email); //retorno verdadeiro ou falso  
+}
+
+function toggleEmailErros(){
+    const email = document.getElementById('email').value;
+    if(!email){
+        document.getElementById('email-required-error').style.display = "block"; 
+        //Alterando propriedade do css para exibir o erro em "bloco"
+    }else{
+        document.getElementById('email-required-error').style.display = "none";
+        //Alterando propriedade do css para não exibir o erro
+    }
+
+    if(validateEmail(email)){
+        document.getElementById('email-invalid-error').style.display = "none";
+        //Alterando propriedade do css para não exibir o erro
+    }else{
+        document.getElementById('email-invalid-error').style.display = "block";
+        //Alterando propriedade do css para exibir o erro em "bloco"
+    }
+}
+
+function togglePasswordError(){
+    const senha = document.getElementById('senha').value;
+    if(!senha){
+        document.getElementById('password-required-error').style.display = "block";
+    }else{
+        document.getElementById('password-required.error').style.display = "none";
+    }
+}
+
+function toggleButtonsDisable(){
+    const emailValid = isEmailValid();
+    document.querySelector(".btRecuperarSenha").disabled = !emailValid; // Habilita/Desabilita campo email
+    // se verdadeiro, entao habilita o botão de recuperar senha
+    // se falso, entao desabilita o botão de recuperar senha
+    const passwordValid = isPasswordValid();
+    document.querySelector(".btEntrar").disabled = !emailValid || !passwordValid; // Habilita/Desabilita campo senha
+    // se verdadeiro, entao habilita o botão de entrar
+    // se falso, entao desabilita o botão de entrar
 }
 
 function isPasswordValid(){ // Validar campo senha
