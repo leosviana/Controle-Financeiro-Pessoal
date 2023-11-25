fcaa71d180fe952c2085e36922f92d9c963c90b2
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-11-25 09:12:38 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,31 @@
-
-function validateFields(){
+function validateFields(){    
     const emailValid = isEmailValid();
-    document.getElementById("btRecuperarSenha").disabled = !emailValid; // Desabilita quando for inválido
-    // Verificar se o email não é vazio e se o email é valido
+    document.querySelector(".btRecuperarSenha").disabled = !emailValid; // Habilita/Desabilita campo email
     // se verdadeiro, entao habilita o botão de recuperar senha
-    // se falso, entao desabilita o botão de recuperar senha    
+    // se falso, entao desabilita o botão de recuperar senha
+    const passwordValid = isPasswordValid();
+    document.querySelector(".btEntrar").disabled = !emailValid || !passwordValid; // Habilita/Desabilita campo senha
+    // se verdadeiro, entao habilita o botão de entrar
+    // se falso, entao desabilita o botão de entrar
 }
 
-function isEmailValid(){ // Validar email
-    const email = document.getElementById("email").value;
-    if(!email){
-        return false;
+function isEmailValid(){ // Validar campo email    
+    const email = document.getElementById("email").value; //verdadeiro
+    if(!email){ //Se email é diferente de preenchido
+        return false; //Retorna falso       
     }
-    return validateEmail(email); 
+    return validateEmail(email); //retorno verdadeiro ou falso  
 }
 
-function validateEmail(email){ // Verifica se o campo email é válido
+function isPasswordValid(){ // Validar campo senha
+    const senha = document.getElementById("senha").value; //verdadeiro
+    if(!senha){ //Se senha é diferente de preenchido
+        return false; //Retorna falso
+    }
+    return true; //Retorna verdadeiro ou falso
+}
+
+function validateEmail(email){ // Verifica se a descrição do email está no padrão
     var parametro = /\S+@\S+\.\S+/;
     return parametro.test(email);
     // texto@texto.com -> true
